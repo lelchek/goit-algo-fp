93bf9d00f6d41380c32cea8058277189581a8338
olha-ninjatech <olha@ninjatech.ai>
2025-12-29 20:27:08 -0500

    update task_2

--- task_2/main.py
+++ task_2/main.py
@@ -1,18 +1,22 @@
 import turtle
+import math
 
 angle = 45
 
+scale = math.sqrt(2) / 2
+init_len = 100
+
 
 def draw_pithagoras_tree(t, branch_len, level):
-    if level == 0:
+    if level == -1:
         return
 
     t.forward(branch_len)
     t.right(angle)
-    draw_pithagoras_tree(t, branch_len * 0.7, level - 1)
+    draw_pithagoras_tree(t, branch_len * scale, level - 1)
 
     t.left(2 * angle)
-    draw_pithagoras_tree(t, branch_len * 0.7, level - 1)
+    draw_pithagoras_tree(t, branch_len * scale, level - 1)
 
     t.right(angle)
     t.backward(branch_len)
@@ -27,13 +31,13 @@ def main():
     t = turtle.Turtle()
     t.speed(1)
     t.color("brown")
+    t.pensize(2)
     t.left(90)
     t.up()
     t.goto(0, -200)
     t.down()
-    t.pensize(2)
 
-    draw_pithagoras_tree(t, 100, recursion_level)
+    draw_pithagoras_tree(t, init_len, recursion_level)
 
     t.hideturtle()
     screen.exitonclick()
